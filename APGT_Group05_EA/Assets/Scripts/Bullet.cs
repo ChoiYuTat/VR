3e5cf560f4b6592dd0ea32822a48ae94bbbf7995
ChoiYuTat <230131555@stu.vtc.edu.hk>
2024-11-24 23:32:14 +0800

    Scripts

---
+++ APGT_Group05_EA/Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject bulletPrefab;
+    GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            bullet = Instantiate(bulletPrefab, cam.transform.position, cam.transform.rotation);
+    }
+}
