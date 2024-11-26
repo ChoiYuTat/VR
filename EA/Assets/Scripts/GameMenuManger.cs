a1b39d7f0d2b459c3fee774ceb744b0773e64a06
LeungOnTik <161811533+LeungOnTik@users.noreply.github.com>
2024-11-26 13:33:25 +0800

    stat buttom test

--- EA/Assets/Scripts/GameMenuManger.cs
+++ EA/Assets/Scripts/GameMenuManger.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GameMenuManger : MonoBehaviour
 {
+
+    public GameObject gameMenu;
+    
+
     // Start is called before the first frame update
     void Start()
     {
